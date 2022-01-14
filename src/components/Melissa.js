b27b30ef089aa7b017455766c098c7850cd1b2d4
MelLeyva <mleyva0795@gmail.com>
2022-01-14 14:33:34 -0600

    notones melissa

---
+++ src/components/Melissa.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Melissa = () => {
+    return (
+        <div>
+            <button type="button" className="btn btn-primary">Primary</button>
+            <button type="button" className="btn btn-secondary">Secondary</button>
+        </div>
+    )
+}
+
+export default Melissa
