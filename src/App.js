b5c3ec2ba52438295512344cf0631793ee9db294
MelLeyva <mleyva0795@gmail.com>
2022-01-16 18:27:11 -0600

    fotter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Melissa from './components/Melissa'
-import './App.css';
-import { Vicki } from './components/Vicki';
-import Berenice from './components/Berenice';
+import Footer from "./components/Footer";
+import './Scss/Layout/footer.scss';
 
 function App() {
   return (
-    <Vicki/>
-    <Berenice />
-    <Melissa />
-  )
+    <>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
