bd6e289ddd1b74804ec3db46a8c414e1cff558c6
berenicehr <bere4758@gmail.com>
2022-01-18 17:03:39 -0600

    3 Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Home } from "./pages/Home";
+/* import { Home } from "./pages/Home"; */
 import "./Scss/Layout/home.scss"
 import { HeaderNav } from './components/HeaderNav';
 import Footer from "./components/Footer";
 import './Scss/Layout/footer.scss';
 import './Scss/Layout/sidebar.scss';
 import './Scss/Layout/HeaderNav.scss';
+import Transfers from "./pages/Transfers";
+import SideBar from "./components/SideBar";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
 
 function App() {
   return (
     <>
     <HeaderNav/>
-    <Home />
+   {/*  <Home /> */}
+    <SideBar/>
+    <Transfers />
+    <Page3 />
     <Footer />
+    <Page4 />
     </>
   );
 }
