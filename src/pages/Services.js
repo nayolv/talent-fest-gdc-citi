b472d724e17488a3806bf4d41ae2729a9fa166a6
Katia <katiacuevasj@gmail.com>
2022-01-18 18:49:41 -0600

    Soluciona ruta

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import { Outlet } from 'react-router-dom'
 
 const Services = () => {
     return (
         <>
-        {/* <SideBar /> */}
+        <SideBar />
+        <Outlet /> 
         </>
+
     )
 }
 
