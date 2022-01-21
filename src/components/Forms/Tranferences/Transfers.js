70b2405ce90f6b5c8c42078d4bf2ce52e645a34e
Victoria Tejeda <vicyatm@gmail.com>
2022-01-20 18:13:11 -0600

    estilos en formularios

--- src/components/Forms/Tranferences/Transfers.js
+++ src/components/Forms/Tranferences/Transfers.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import '../../../Scss/Layout/transfer.scss'
-import SideBar from '../../SideBar'
+import React from "react";
+import "../../../Scss/Layout/transfer.scss";
+import SideBar from "../../SideBar";
 
 const Transfers = () => {
-    return (
-
-        <>
+  return (
+    <>
+      <h1 className="entry-question"> ¿Qué deseas hacer?</h1>
+      <hr className="line"/>
+      <section className="container-saider-form">
         <SideBar />
-        <form>
-        <h1>Ingresa los datos de la operación</h1>    
-        <p>Podrás transferir a cualquier banco, pagar tarjetas de crédito y servicios, y órdenes de pago.</p>
-        <br />
-        <br />
-        <div className ="container">
-            <div className ="col-md-5">
-                    <label>Cuenta de retiro:</label>
-                    <select name="seleccione una opción" className="form-control" placeholder="Seleccione una opción">
-                    <option defaultValue="Seleccione una opción">Seleccione una opción</option>
-                    </select>
+        <form className="all-form">
+          <h1>Ingresa los datos de la operación</h1>
+          <p>
+            Podrás transferir a cualquier banco, pagar tarjetas de crédito y
+            servicios, y órdenes de pago.
+          </p>
+          <br />
+          <br />
+          <div className="container">
+            <div className="col-md-5">
+              <label>Cuenta de retiro:</label>
+              <select className="form-select" id="inputGroupSelect01">
+                <option defaultValue="Seleccione una opción">
+                  Seleccione una opción
+                </option>
+              </select>
             </div>
-            <div className ="col-md-5">
-                    <label>Cuenta de deposito:</label>
-                    <select name="seleccione una opción" className="form-control" placeholder="Seleccione una opción">
-                    <option defaultValue="Seleccione una opción">Seleccione una opción</option>
-                    </select>
+            <div className="col-md-5">
+              <label>Cuenta de deposito:</label>
+              <select className="form-select" id="inputGroupSelect01">
+                <option defaultValue="Seleccione una opción">
+                </option>
+              </select>
             </div>
-        </div>  
-        <button className="cancel"> Actualizar cuenta(s)</button>
-        <hr />
-        <div className ="transferButtons">
-        <button type="button" className="continue">Continuar</button>
-        <button type="button" className="cancel">Cancelar</button>
-        </div>
+          </div>
+          <button className="update-account"> <i className="bi bi-caret-right-fill"></i>  Actualizar cuenta(s)</button>
+          <div className="transferButtons">
+            <button type="button" className="continue">
+              Continuar
+            </button>
+            <button type="button" className="cancel">
+              <i class="bi bi-caret-right-fill"></i>Cancelar
+            </button>
+          </div>
         </form>
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
 
-export default Transfers
+export default Transfers;
