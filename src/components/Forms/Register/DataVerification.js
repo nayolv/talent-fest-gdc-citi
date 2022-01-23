e1ee26337885ae58de8255cde99446f987202f28
MelLeyva <mleyva0795@gmail.com>
2022-01-23 14:37:26 -0600

    styles register

--- src/components/Forms/Register/DataVerification.js
+++ src/components/Forms/Register/DataVerification.js
@@ -3,14 +3,13 @@ import SideBar from "../../SideBar";
 import { useNavigate } from "react-router-dom";
 import { dataApi } from "../../../api/dataApi";
 
-
-const DataVerification = ({ dataRegister, checked}) => {
-  const navigate = useNavigate()
+const DataVerification = ({ dataRegister, checked }) => {
+  const navigate = useNavigate();
 
   //console.log(dataRegister);
   //console.log(checked)
 
-/*   const newCard = {
+  /*   const newCard = {
     client: dataRegister.client,
       name: dataRegister.name,
       displayAccountNumber: dataRegister.displayAccountNumber,
@@ -21,17 +20,15 @@ const DataVerification = ({ dataRegister, checked}) => {
  */
   //console.log(newCard)
 
-
-
   const postDataAccount = async () => {
     await dataApi.post("https://api-talent-fest.herokuapp.com/clientData", {
       id: "",
       client: dataRegister.client,
       name: dataRegister.name,
       displayAccountNumber: dataRegister.displayAccountNumber,
-      maximumAmount:dataRegister.maximumAmount,
+      maximumAmount: dataRegister.maximumAmount,
       email: dataRegister.email,
-      typeAccount: checked
+      typeAccount: checked,
     });
   };
 
@@ -41,41 +38,42 @@ const DataVerification = ({ dataRegister, checked}) => {
       <hr />
       <section className="container-saider-form">
         <SideBar />
-        <section>
-          <table className="default">
+        <section className="container-data">
+          <h2>Alta de cuenta Citibanamex</h2>
+          <p>
+            Confirma los datos que ingresaste y si son correctos, haz clic en
+            "Aceptar" para enviar tu operación.
+          </p>
+          <table className="corfirm-data">
             <tbody>
-            <tr>
-              <td>Basado en:</td>
-              <td>Tarjeta de credito o débito</td>
-            </tr>
-            <tr>
-              <td>Plan bancario:</td>
-              <td>{dataRegister.name}</td>
-            </tr>
-            <tr>
-              <td>Número de tarjeta:</td>
-              <td>{dataRegister.displayAccountNumber}</td>
-            </tr>
-            <tr>
-              <td>Titular:</td>
-              <td>{dataRegister.client}</td>
-            </tr>
-            <tr>
-              <td>Monto máximo que autorizas pagar a este beneficiario:</td>
-              <td>{dataRegister.maximumAmount}</td>
-            </tr>
-            <tr>
-              <td>Correo electrónico del beneficiario:</td>
-              <td>{dataRegister.email}</td>
-            </tr>
+              <tr>
+                <td>Basado en:</td>
+                <td>Tarjeta de credito o débito</td>
+              </tr>
+              <tr>
+                <td>Plan bancario:</td>
+                <td>{dataRegister.name}</td>
+              </tr>
+              <tr>
+                <td>Número de tarjeta:</td>
+                <td>{dataRegister.displayAccountNumber}</td>
+              </tr>
+              <tr>
+                <td>Titular:</td>
+                <td>{dataRegister.client}</td>
+              </tr>
+              <tr>
+                <td>Monto máximo que autorizas pagar a este beneficiario:</td>
+                <td>{dataRegister.maximumAmount}</td>
+              </tr>
+              <tr>
+                <td>Correo electrónico del beneficiario:</td>
+                <td>{dataRegister.email}</td>
+              </tr>
             </tbody>
           </table>
           <div className="transferButtons">
-            <button
-              type="button"
-              className="accept"
-              onClick={postDataAccount}
-            >
+            <button type="button" className="accept" onClick={postDataAccount}>
               Aceptar
             </button>
             <button
