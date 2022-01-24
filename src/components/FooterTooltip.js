ec12837ff54e52f2db8beb8ea3235218e997e8b5
Katia <katiacuevasj@gmail.com>
2022-01-24 01:04:54 -0600

    tooltipFooter

--- src/components/FooterTooltip.js
+++ src/components/FooterTooltip.js
@@ -14,16 +14,16 @@ const FooterTooltip = () => {
             Citibanamex.com
           </a>
           <div className="links">
-            <a href="https://bancanet.banamex.com/JFP/regional/documents/contrato_marzo_2011.pdf" className="link-primary">
+            <a href="https://bancanet.banamex.com/JFP/regional/documents/contrato_marzo_2011.pdf" className="link-primary" id="tooltip" data-info="Ver contrato de Banca Electrónica">
               Ver contrato de Banca Electrónica
             </a>
-            <a href="https://www.banamex.com/es/terminos_condiciones.htm?icid=Texto-TerminosCondiciones-Footer-Avatar-05282015-Int-ES" className="link-primary">
+            <a href="https://www.banamex.com/es/terminos_condiciones.htm?icid=Texto-TerminosCondiciones-Footer-Avatar-05282015-Int-ES" className="link-primary" id="tooltip" data-info="Términos, condiciones y aviso de privacidad">
               Términos, condiciones y aviso de privacidad
             </a>
           </div>
         </div>
         <div className="autor d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <span className="text-muted">© 2022 GDC, Talent-Fest</span>
+          <span className="text-muted" id="tooltip" data-info="© 2022 GDC, Talent-Fest">© 2022 GDC, Talent-Fest</span>
         </div>
       </footer>
     </div>
--- src/components/OfertasHomeTooltip.js
+++ src/components/OfertasHomeTooltip.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { useTransfers } from "../hooks/useTransfers";
-import '../Scss/Layout/Tooltip.scss'
+import "../Scss/Layout/Tooltip.scss";
 
 export const OfertasHomeTooltip = () => {
- const { getDataTransfer } = useTransfers();
- const ownAccounts = getDataTransfer.filter((item) => item.typeAccount=== true);
+  const { getDataTransfer } = useTransfers();
+  const ownAccounts = getDataTransfer.filter(
+    (item) => item.typeAccount === true
+  );
 
- return (
+  return (
     <div className="container-home ">
       <section className="offers-container">
-        <h2 id="tooltip" data-info="Ofertas Citibanamex">Ofertas Citibanamex</h2>
+        <h2 id="tooltip" data-info="Ofertas Citibanamex">
+          Ofertas Citibanamex
+        </h2>
 
         <section className="cards-offer-home">
-
           <section className="card-home">
             <img
               src="https://i.ibb.co/WF72Xmr/Bobby-brown.png"
               alt="Oferta Bobby Brown, 15% de descuento"
             />
             <section className="text-promotion-container">
-              <p className="title-card-home" id="tooltip" data-info="Bobby Brown">Bobby Brown</p>
-              <p id="tooltip" data-info="15% de descuento">15% de descuento</p>
+              <p
+                className="title-card-home"
+                id="tooltip"
+                data-info="Bobby Brown"
+              >
+                Bobby Brown
+              </p>
+              <p id="tooltip" data-info="15% de descuento">
+                15% de descuento
+              </p>
             </section>
           </section>
 
@@ -30,8 +41,16 @@ export const OfertasHomeTooltip = () => {
               alt="Oferta pase anual 6 meses sin intereses al inscribirte"
             />
             <section className="text-promotion-container">
-              <p className="title-card-home" id="tooltip" data-info="Pase Anual Citibanamex">Pase Anual Citibanamex</p>
-              <p id="tooltip" data-info="Inscríbete y obtén 6 MSI">Inscríbete y obtén 6 MSI</p>
+              <p
+                className="title-card-home"
+                id="tooltip"
+                data-info="Pase Anual Citibanamex"
+              >
+                Pase Anual Citibanamex
+              </p>
+              <p id="tooltip" data-info="Inscríbete y obtén 6 MSI">
+                Inscríbete y obtén 6 MSI
+              </p>
             </section>
           </section>
         </section>
@@ -45,46 +64,67 @@ export const OfertasHomeTooltip = () => {
       </section>
 
       <section className="container-acconts">
-      <p id="tooltip" data-info="Cuentas de efectivo">Cuentas de efectivo</p>
+        <p id="tooltip" data-info="Cuentas de efectivo">
+          Cuentas de efectivo
+        </p>
 
-      {ownAccounts.map(item=>(
-        <section key={item.id} className="acconts-client-home-container">
-        <section className="card-home-container">
-          <img
-            src="https://www.banamex.com/landings/tarjetas-de-debito/img/desktop/PerfilEjecutivo.png"
-            alt="tarjeta banamex perfil ejecutivo"
-          />
-          <section>
-            <a href="./cualquiercosa" id="tooltip" data-info={`${item.name}-MXN`}>{item.name}-MXN</a>
-            <p className="account-number" id="tooltip" data-info={`**${item.displayAccountNumber.slice(-3)}`}>{item.displayAccountNumber.slice(-3)}</p>
-          </section>
-        </section>
+        {ownAccounts.map((item) => (
+          <section key={item.id} className="acconts-client-home-container">
+            <section className="card-home-container">
+              <img
+                src="https://www.banamex.com/landings/tarjetas-de-debito/img/desktop/PerfilEjecutivo.png"
+                alt="tarjeta banamex perfil ejecutivo"
+              />
+              <section>
+                <a
+                  href="./cualquiercosa"
+                  id="tooltip"
+                  data-info={`${item.name}-MXN`}
+                >
+                  {item.name}-MXN
+                </a>
+                <p
+                  className="account-number"
+                  id="tooltip"
+                  data-info={`**${item.displayAccountNumber.slice(-3)}`}
+                >
+                  {item.displayAccountNumber.slice(-3)}
+                </p>
+              </section>
+            </section>
 
-        <section className="avalaible-container">
-          <p id="tooltip" data-info="Disponible:">Disponible:</p>
-          <section>
-<<<<<<< HEAD
-            <p className="balance">{item.maximumAmount}</p> 
-            <p id="tooltip" data-info={`${item.maximumAmount}MXN`}>MXN</p>
-=======
-            <p className="balance" id="tooltip" data-info={`**${item.maximumAmount}`}>{item.maximumAmount}</p> 
-            <p id="tooltip" data-info="MXN">MXN</p>
->>>>>>> 6f193e83b6f80669da615385b3681a2175473753
-          </section>
-        </section>
+            <section className="avalaible-container">
+              <p id="tooltip" data-info="Disponible:">
+                Disponible:
+              </p>
+              <section>
+                <p className="balance">{item.maximumAmount}</p>
+                <p id="tooltip" data-info={`${item.maximumAmount}MXN`}>
+                  MXN
+                </p>
+              </section>
+            </section>
 
-        <section className="avalaible-container">
-          <p id="tooltip" data-info="Saldo:">Saldo:</p>
-          <section>
-            <p className="balance" id="tooltip" data-info={`**${item.balance}`}>{item.balance}</p> 
-            <p id="tooltip" data-info="MXN">MXN</p>
+            <section className="avalaible-container">
+              <p id="tooltip" data-info="Saldo:">
+                Saldo:
+              </p>
+              <section>
+                <p
+                  className="balance"
+                  id="tooltip"
+                  data-info={`**${item.balance}`}
+                >
+                  {item.balance}
+                </p>
+                <p id="tooltip" data-info="MXN">
+                  MXN
+                </p>
+              </section>
+            </section>
           </section>
-        </section>
+        ))}
       </section>
-      ))}
-
-     </section>
-
     </div>
   );
 };
