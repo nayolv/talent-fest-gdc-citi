4bfb200be1cb3fce7a2b2972d28614a3eee71d47
Nayelli <nattkeres@gmail.com>
2022-01-23 23:34:52 -0600

    Boton zoom texto implementado

--- src/components/MainTooltip.js
+++ src/components/MainTooltip.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { LateralMenuHome } from "./LateralMenuHome";
 import { ModalSia } from "./modal/ModalSia";
-import { OfertasHome } from "./OfertasHome";
- import { ThemeContext} from "./modal/Context" 
+import { ThemeContext } from "./modal/Context";
 import { OfertasHomeTooltip } from "./OfertasHomeTooltip";
 import { LateralMenuHomeTooltip } from "./LateralMenuHomeTooltip";
 
 export const Maintooltip = ({ resultRetirement, isTooltipOn }) => {
-
-  const {changeColor, setChangeColor} = React.useContext(ThemeContext);
+  const { changeColor, setChangeColor } = React.useContext(ThemeContext);
 
   return (
-
-    <main style = { {background: changeColor.background} } >
-      <div className="main-container" >
-      <OfertasHomeTooltip resultRetirement={resultRetirement} />
-      <LateralMenuHomeTooltip />
-      <ModalSia isTooltipOn={ isTooltipOn } />
+    <main style={{ background: changeColor.background }}>
+      <div className="main-container">
+        <OfertasHomeTooltip resultRetirement={resultRetirement} />
+        <LateralMenuHomeTooltip />
+        <ModalSia isTooltipOn={isTooltipOn} />
       </div>
-      
     </main>
   );
 };
-
