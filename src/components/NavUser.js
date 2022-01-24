0e2f4bef43db133e6f56b9f5433954d7ea05c6e6
Victoria Tejeda <vicyatm@gmail.com>
2022-01-24 14:15:00 -0600

    responsive nav

--- src/components/NavUser.js
+++ src/components/NavUser.js
@@ -1,48 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const NavUser = () => {
-    return (
-  
-        <section className="shadow p-3 mb-5 bg-white container-nav-user">  
-        <ul className="nav nav-pills nav-fill">
-          <li className="nav-item">
-          <i className="bi bi-house-door"></i>
-            <p className="nav-home" aria-current="page">
-            <Link to="/"> Inicio </Link> 
-            </p>
-          </li>
-          <li className="nav-item">
-          <i className="bi bi-coin"></i>
-            <p className="nav-transfer">
-            <Link to="services"> Transferencias y pagos </Link>
-            </p>
-          </li>
-          <li className="nav-item">
-          <i className="bi bi-graph-up-arrow"></i>
-            <Link className="nav-investment" to="#">
-              Inversiones
-            </Link>
-          </li>
-          <li className="nav-item">
-          <i className="bi bi-credit-card"></i>
-            <p className="nav-others">
-             Otros productos
-            </p>
-          </li>
-          <li className="nav-item">
-          <i className="bi bi-gear"></i>
-            <p className="nav-setting">
-              Configuración
-            </p>
-          </li>
-          <li className="nav-item">
-          <i className="bi bi-question-circle"></i>
-            <p className="nav-help">
-              Ayuda
-            </p>
-          </li>
-        </ul>
-      </section>
-    )
-}
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg shadow p-3 mb-5 bg-white container-nav-user">
+        <div className="container nav nav-pills nav-fill">
+          <ul className=" nav-user ms-auto">
+            <li className="nav-item">
+              <i className="bi bi-house-door"></i>
+              <p className="nav-home" aria-current="page">
+                <Link to="/"> Inicio </Link>
+              </p>
+            </li>
+            <li className="nav-item">
+              <i className="bi bi-coin"></i>
+              <p className="nav-transfer">
+                <Link to="services"> Transferencias y pagos </Link>
+              </p>
+            </li>
+          </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="nav nav-pills nav-fill">
+              <li className="nav-item">
+                <i className="bi bi-graph-up-arrow"></i>
+                <p className="nav-others">Otros productos</p>
+              </li>
+              <li className="nav-item">
+                <i className="bi bi-credit-card"></i>
+                <p className="nav-others">Otros productos</p>
+              </li>
+              <li className="nav-item">
+                <i className="bi bi-gear"></i>
+                <p className="nav-setting">Configuración</p>
+              </li>
+              <li className="nav-item help">
+                <i className="bi bi-question-circle"></i>
+                <p className="nav-help">Ayuda</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
