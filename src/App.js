fe74657ef4a6e72f287a1adcb0a49abfc685723c
Nayelli <nattkeres@gmail.com>
2022-01-24 21:54:18 -0600

    Accesibilidad completap

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import "./Scss/Layout/footer.scss";
 import "./Scss/Layout/sidebar.scss";
 import "./Scss/Layout/HeaderNav.scss";
 import "../src/App.css";
-import './Scss/Layout/CorfimRegister.scss';
-import './Scss/Layout/Tooltip.scss';
+import "./Scss/Layout/CorfimRegister.scss";
+import "./Scss/Layout/Tooltip.scss";
 import { Home } from "./pages/Home";
 import { HeaderNav } from "./components/HeaderNav";
 import Footer from "./components/Footer";
@@ -33,7 +33,7 @@ function App() {
     handleInputChange,
     checked,
     handleChecked,
-    postDataAccount 
+    postDataAccount,
   } = useRegister();
 
   const {
@@ -59,157 +59,155 @@ function App() {
     recoveryDescription,
     hora,
     hour,
-    getHour
-
+    getHour,
   } = useTransfers();
 
   const [tooltipOn, setTooltipOn] = useState(true);
 
-    const isTooltipOn = () => {
-        setTooltipOn(!tooltipOn);
-    }
+  const isTooltipOn = () => {
+    setTooltipOn(!tooltipOn);
+  };
 
   return (
     <>
-    <Context>
-      <Router>
-        {/* {tooltipOn ? (<HeaderNav getDataTransfer={getDataTransfer} />):(<HeaderNavTooltip getDataTransfer={getDataTransfer} />)} */}
-        <HeaderNav getDataTransfer={getDataTransfer} tooltipOn={tooltipOn} />
-        <Routes>
-      {/*    {tooltipOn ? ( <Route
-            path="/"
-            element={<Home resultRetirement={resultRetirement} isTooltipOn={isTooltipOn} />}
-          />) : ( <Route
-            path="/"
-            element={<HomeTootltip resultRetirement={resultRetirement} isTooltipOn={isTooltipOn} />}
-          />)} */}
-          <Route path="/" element={<Home resultRetirement={resultRetirement} isTooltipOn={isTooltipOn} tooltipOn={tooltipOn} />}/>
-          <Route path="services/" element={<Services />}>
+      <Context>
+        <Router>
+          <HeaderNav getDataTransfer={getDataTransfer} tooltipOn={tooltipOn} />
+          <Routes>
             <Route
-              index
+              path="/"
               element={
-                <Transfers
-                  recoverySelectValueRet={recoverySelectValueRet}
-                  recoverySelectValue={recoverySelectValue}
-                  selectValue={selectValue}
-                  getDataTransfer={getDataTransfer}
-                  result={result}
+                <Home
                   resultRetirement={resultRetirement}
-                  mapeoRetirement={mapeoRetirement}
-                  mapeo={mapeo}
                   isTooltipOn={isTooltipOn}
                   tooltipOn={tooltipOn}
+                />
+              }
+            />
+            <Route path="services/" element={<Services />}>
+              <Route
+                index
+                element={
+                  <Transfers
+                    recoverySelectValueRet={recoverySelectValueRet}
+                    recoverySelectValue={recoverySelectValue}
+                    selectValue={selectValue}
+                    getDataTransfer={getDataTransfer}
+                    result={result}
+                    resultRetirement={resultRetirement}
+                    mapeoRetirement={mapeoRetirement}
+                    mapeo={mapeo}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                exact
+                path="register"
+                element={
+                  <DataRegister
+                    typeRegister={typeRegister}
+                    newRegister={newRegister}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                path="own-account"
+                element={
+                  <OwnAccount
+                    handleChange={handleChange}
+                    importe={importe}
+                    mapeoRet={mapeoRet}
+                    mapeoDep={mapeoDep}
+                    hora={hora}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                path="verification-transfer"
+                element={
+                  <Verification
+                    mapeoRet={mapeoRet}
+                    mapeoDep={mapeoDep}
+                    importe={importe}
+                    balanceDeposito={balanceDeposito}
+                    newBalance={newBalance}
+                    descriptionTransfer={descriptionTransfer}
+                    getHour={getHour}
+                    hora={hora}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                path="confirmation"
+                element={
+                  <Confirmation
+                    mapeoRet={mapeoRet}
+                    mapeoDep={mapeoDep}
+                    importe={importe}
+                    balanceDep={balanceDep}
+                    balance={balance}
+                    patchApiOwn={patchApiOwn}
+                    patchApiOwnDep={patchApiOwnDep}
+                    descriptionTransfer={descriptionTransfer}
+                    hour={hour}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
 
-              }
-            />
-            <Route
-              exact
-              path="register"
-              element={
-                <DataRegister
-                  typeRegister={typeRegister}
-                  newRegister={newRegister}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-                />
-              }
-            />
-            <Route
-              path="own-account"
-              element={
-                <OwnAccount
-                  handleChange={handleChange}
-                  importe={importe}
-                  mapeoRet={mapeoRet}
-                  mapeoDep={mapeoDep}
-                  hora={hora}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-                />
-              }
-            />
-            <Route
-              path="verification-transfer"
-              element={
-                <Verification
-                  mapeoRet={mapeoRet}
-                  mapeoDep={mapeoDep}
-                  importe={importe}
-                  balanceDeposito={balanceDeposito}
-                  newBalance={newBalance}
-                  descriptionTransfer={descriptionTransfer}
-                  getHour={getHour}
-                  hora={hora}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-                />
-              }
-            />
-            <Route
-              path="confirmation"
-              element={
-                <Confirmation
-                  mapeoRet={mapeoRet}
-                  mapeoDep={mapeoDep}
-                  importe={importe}
-                  balanceDep={balanceDep}
-                  balance={balance}
-                  patchApiOwn={patchApiOwn}
-                  patchApiOwnDep={patchApiOwnDep}
-                  descriptionTransfer={descriptionTransfer}
-                  hour={hour}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-
-                />
-              }
-            />
-
-            <Route
-              path="third-account"
-              element={
-                <ThirdAccount
-                  handleChange={handleChange}
-                  importe={importe}
-                  mapeoRet={mapeoRet}
-                  mapeoDep={mapeoDep}
-                  recoveryDescription={recoveryDescription}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-                />
-              }
-            />
-
-            <Route
-              path="formRegister"
-              element={
-                <AccountRegister
-                  sendData={sendData}
-                  dataRegister={dataRegister}
-                  handleInputChange={handleInputChange}
-                  checked={checked}
-                  handleChecked={handleChecked}
-                  isTooltipOn={isTooltipOn}
-                  tooltipOn={tooltipOn}
-                />
-              }
-            />
-            <Route
-              path="verification"
-              element={
-                <DataVerification
-                dataRegister={dataRegister}
-                checked={checked}
-                postDataAccount ={postDataAccount}
-                isTooltipOn={isTooltipOn}
-                tooltipOn={tooltipOn}
-                />
-              }
-            />
-          </Route>
-        </Routes>
-        <Footer tooltipOn={tooltipOn} />
-      </Router>
+              <Route
+                path="third-account"
+                element={
+                  <ThirdAccount
+                    handleChange={handleChange}
+                    importe={importe}
+                    mapeoRet={mapeoRet}
+                    mapeoDep={mapeoDep}
+                    recoveryDescription={recoveryDescription}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                path="formRegister"
+                element={
+                  <AccountRegister
+                    sendData={sendData}
+                    dataRegister={dataRegister}
+                    handleInputChange={handleInputChange}
+                    checked={checked}
+                    handleChecked={handleChecked}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+              <Route
+                path="verification"
+                element={
+                  <DataVerification
+                    dataRegister={dataRegister}
+                    checked={checked}
+                    postDataAccount={postDataAccount}
+                    isTooltipOn={isTooltipOn}
+                    tooltipOn={tooltipOn}
+                  />
+                }
+              />
+            </Route>
+          </Routes>
+          <Footer tooltipOn={tooltipOn} />
+        </Router>
       </Context>
     </>
   );
